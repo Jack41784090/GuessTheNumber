e9a64ba22f6592f3a10b7e01358935c791dd1966
Jack <tszmingjack@gmail.com>
2022-06-05 13:28:39 -0700

    Guessing number now gives a hint (higher or lower)

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
 
 
-def guess_number(correct_number):
+def guess_number(max_number):
     correct = False
+    random_number = random.randint(1, max_number)
     while not correct:
-        guessed = int(input(f'Guess a number:'))
-        print(guessed)
-        if correct_number == guessed:
+        guessed = int(input(f'Guess a number between 1 and {max_number}:'))
+        if random_number > guessed:
+            print(f'Go higher!')
+        elif random_number < guessed:
+            print(f'Go lower!')
+        elif random_number == guessed:
             correct = True
-        print(f'You guessed: {guessed}')
-    print(f'... and it\'s correct!')
+        print(f'You guessed {guessed} and it is { "correct" if correct else "incorrect" }')
 
 
 guess_number(10)
