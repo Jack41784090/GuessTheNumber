094b75ae79f8eae7ef447ab6fc8259c149f9edb3
Jack <tszmingjack@gmail.com>
2022-06-04 22:48:18 -0700

    guess a number

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def guess_number(correct_number):
+    correct = False
+    while not correct:
+        guessed = int(input(f'Guess a number:'))
+        print(guessed)
+        if correct_number == guessed:
+            correct = True
+        print(f'You guessed: {guessed}')
+    print(f'... and it\'s correct!')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+guess_number(10)
